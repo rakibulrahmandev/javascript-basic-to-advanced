2cddae51fcf3efb798a8847fde16d17b157bf835
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-06-21 22:26:07 +0600

    javascript datatypes on 06/21/2025 at 10:25 pm

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,52 @@
+//? Javascript data types ------------------------------------------------------------------->
+
+// Six type of data in javascript like 'number', 'string', 'boolean', 'null', 'undefined', 'bigint', and 'symbol';
+
+// let's see how looks 'number' data types:
+const num = 10;
+
+// let's see how looks 'string' data types:
+const str = "Rakibul Rahman";
+
+// let's see how looks 'boolean' data types:
+const boolean = true;
+
+// let's see how looks 'null' data types:
+const notAvailable = null;
+
+// let's see how looks 'undefined' data types:
+const noValue = undefined;
+
+// And other two data types are not use regularly. most of the case we use this 'bigint' and 'symbol' data types;
+
+// let's check the data types: (here we use the 'typeof' keywords to see what is the type in this data. this is javascript build in keywords.);
+console.log(typeof num); // output: data type of 'number';
+console.log(typeof str); // output: data type of 'string';
+console.log(typeof boolean); // output: data type of 'boolean';
+console.log(typeof notAvailable); // output: data type of 'object'; (when we see the 'null' data types, we see 'null' is an 'object'. because this is bugs on javascript.)
+console.log(typeof noValue); // output: data type of 'undefined';
+
+/**
+ * let's talk about 'string' data type:
+ * 1. when we declare a 'string' type data than we want to double or single quotation like ('rakibul rahman', "rakibul rahman");
+*/
+
+/**
+ * let's talk about 'number' data type:
+ * 1. when we declare a 'number' type data we not need any quotation just declare like (1254523);
+*/
+
+/**
+ * let's talk about 'boolean' data type:
+ * 1. when we declare a 'boolean' type data just we say 'true' or 'false' this is boolean data; 
+*/
+
+/**
+ * let's talk about 'null' data type:
+ * 1. when we declare a 'null' type data it's just an object because this is the bugs of javascript; 
+*/
+
+/**
+ * let's talk about 'undefined' data type:
+ * 1. when we declare a 'undefined' type data is 'undefined'; 
+*/
