b111a24ca7eee095fb1d44c2ba6ea4644a8bbff4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-08 15:44:57 +0600

    javascript truthy and falsy values on 07/08/2025 at 03:44 pm

---
+++ 04_javascript_control_flow/05_truthy_and_falsy.js
@@ -0,0 +1,23 @@
+//? javascript truthy and falsy values ------------------------------------------------------------------->
+
+// let's go to learn truthy and falsy values:
+
+/**
+ * let's break down truthy values:
+ * 01. true
+ * 02. 1, -1
+ * 03. "0", "false", "hi" (non-empty strings)
+ * 04. [], {} (empty array and object)
+ * 05. Infinity, -Infinity
+ * 06. function() {}
+*/
+
+/**
+ * let's break down falsy values:
+ * 01. false
+ * 02. 0, -0
+ * 03. 0n (BigInt zero like: BigInt 0n)
+ * 04. "" (empty string)
+ * 05. undefined
+ * 06. NaN
+*/ 
