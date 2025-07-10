6db87da5dd07d0fc267adc2ad716a88095b43f2e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-10 10:41:53 +0600

    javascript array reduce on 07/10/2025 at 10:41 am

--- 06_iteration/03_array_reduce.js
+++ 06_iteration/03_array_reduce.js
@@ -17,3 +17,5 @@
 const numbers = [10, 25, 1500, 400, 800, 20000];
 const total = numbers.reduce((acc, curr) => acc + curr, 0);
 console.log(total); // output: 22735;
+
+// note: The reduce() method processes an array and returns a single output value using an accumulator.
