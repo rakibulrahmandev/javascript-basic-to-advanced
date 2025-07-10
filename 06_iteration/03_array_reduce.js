42049dffefdf2c29ce1017ddd8356dd71188fb72
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-10 10:41:32 +0600

    javascript array reduce on 07/10/2025 at 10:41 am

---
+++ 06_iteration/03_array_reduce.js
@@ -0,0 +1,19 @@
+//? javascript array reduce ------------------------------------------------------------------->
+
+// let's go to learn javascript array reduce:
+
+/**
+ * let's see what we learn:
+ * 01. basic syntax of array reduce
+ * 02. reduce array element
+*/
+
+// basic syntax of array reduce:
+/**
+ * array.reduce(callback(accumulator, currentValue, index, array), initialValue)
+*/
+
+// reduce array element:
+const numbers = [10, 25, 1500, 400, 800, 20000];
+const total = numbers.reduce((acc, curr) => acc + curr, 0);
+console.log(total); // output: 22735;
